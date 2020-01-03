7dbf7eab7f1c6b812b7bdb527f73f08e1a4463ca
lab302-35 <lab302-35@labeng.local>
2020-01-03 15:22:26 +0700

    Alpha

--- lab4_1.cpp
+++ lab4_1.cpp
@@ -1,1 +1,13 @@
-
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main (){
+ double e = 2.71828 , y;
+ int x ;
+ cout << "Enter x = ";
+ cin >> x ;
+ y= 3*pow(x,3) + 2*e +pow(2,((2*x)+1))+sqrt(pow(x,2)+1);
+ cout << "Result y = " << y ;
+return 0;
+}
+ 
