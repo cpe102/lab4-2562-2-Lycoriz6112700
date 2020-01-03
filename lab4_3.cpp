7f3ca4f640b4397c4dffa8e5e8b03844cf6014a7
lab302-35 <lab302-35@labeng.local>
2020-01-03 16:42:31 +0700

    Alpha

--- lab4_3.cpp
+++ lab4_3.cpp
@@ -1,1 +1,32 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+ int findDivisor(int Num,int W ){
+        if(Num > 1){
+            if(Num%1 == 0){
+                while (Num > 1)
+    {
+        if(Num%W == 0){
+            return W;
+        }else{
+            W = W + 1;
+        }
+    };
+            }
+        }else{
+;
+        }
+    
+ }
+ int main(){
+int x , y =2;
+cin >> x ;
+cout << findDivisor(x,y) ;
 
+return 0;
+}
+  
+ 
+
+
+ 
